c931c8380ae9127d463ab002b6eecae121135e68
Adetunji <ashennaike@gmail.com>
2019-04-11 18:49:50 -0400

    finished actions

--- friends/src/actions/Index.js
+++ friends/src/actions/Index.js
@@ -12,7 +12,7 @@ export let FAIL = 'FAIL'
 export const grabData = () => dispatch => {
   dispatch ({ type: FETCH })
 
-  axios.get('http://localhost:5000/friends')
+  axios.get('http://localhost:5000/api/friends')
   .then( res => {
     dispatch({ type: SUCCESS, payload: res.data })
   })
@@ -21,3 +21,22 @@ export const grabData = () => dispatch => {
   })
 }
 
+
+//login exports
+export let LOGIN_START = 'LOGIN_START'
+export let LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export let LOGIN_FAIL = 'LOGIN_FAIL'
+
+
+export const login = () => dispatch => {
+  dispatch ({ type: LOGIN_START })
+
+  axios.get('http://localhost:5000/api/login', credentials)
+  .then( res => {
+    localStorage.setItem('token', res.data.payload)
+    dispatch({ type: LOGIN_SUCCESS, payload: res.data.payload })
+  })
+  .catch( err => {
+    dispatch({ type: LOGIN_FAIL, payload: err})
+  })
+}
